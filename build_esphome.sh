352eb1110e0fd8c8cdd92ee96c1135d60946a4c1
root <root@core-configurator>
2023-08-24 21:11:07 -0400

    Correct build paths to avoid redoing everything if already there from web build

--- build_esphome.sh
+++ build_esphome.sh
@@ -1,17 +1,17 @@
 docker exec -it ab66665fa6d8 bash
-cd config/esphome
+
 export ESPHOME_IS_HA_ADDON=true
 pio_cache_base=/data/cache/platformio
 export PLATFORMIO_PLATFORMS_DIR="${pio_cache_base}/platforms"
 export PLATFORMIO_PACKAGES_DIR="${pio_cache_base}/packages"
 export PLATFORMIO_CACHE_DIR="${pio_cache_base}/cache"
 export PLATFORMIO_GLOBALLIB_DIR=/piolibs
-for config in *.yaml
+for config in /config/esphome/*.yaml
 do
   echo "----- COMPILE ----- $config -----"
   if esphome compile $config
   then 
     echo "----- UPLOAD ----- $config -----"
-    esphome upload $config --device ${config%.*}.local
+    esphome upload $config
   fi
 done
