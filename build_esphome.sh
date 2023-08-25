ab7a3d49a3284c18899a8739b3ae9ab73ebc37be
root <root@core-configurator>
2023-08-24 21:02:51 -0400

    Add some more stuff to the script to make it exactly same as web

--- build_esphome.sh
+++ build_esphome.sh
@@ -1,7 +1,12 @@
 docker exec -it ab66665fa6d8 bash
 cd config/esphome
 export ESPHOME_IS_HA_ADDON=true
-for config in smoke-*.yaml
+pio_cache_base=/data/cache/platformio
+export PLATFORMIO_PLATFORMS_DIR="${pio_cache_base}/platforms"
+export PLATFORMIO_PACKAGES_DIR="${pio_cache_base}/packages"
+export PLATFORMIO_CACHE_DIR="${pio_cache_base}/cache"
+export PLATFORMIO_GLOBALLIB_DIR=/piolibs
+for config in *.yaml
 do
   echo "----- COMPILE ----- $config -----"
   if esphome compile $config
